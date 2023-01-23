a7bb5a6b50d0dd8730d14c5f7ecc815298005441
Rishikesh K V <rishikeshkaroth@gmail.com>
2023-01-24 02:17:26 +0530

    completed the Form section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { logo } from "./assets";
-import { Home, CreatePost } from "./assets/pages";
+import { Home, CreatePost } from "./pages";
 const App = () => {
   return (
     <BrowserRouter>
