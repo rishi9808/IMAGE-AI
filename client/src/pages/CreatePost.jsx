102388c7cd360d5342ad8e2dde0720cb262b829d
Rishikesh K V <rishikeshkaroth@gmail.com>
2023-02-03 02:28:01 +0530

    updated https  wrt to deployed server

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -22,7 +22,7 @@ const CreatePost = () => {
     if(form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('http://localhost:8000/api/v1/dalle',{
+        const response = await fetch('https://dall-e-ae5t.onrender.com/api/v1/dalle',{
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
