a05f6bf7e699e53a0d9bdd646ff99f154bfb1614
Rishikesh K V <rishikeshkaroth@gmail.com>
2023-02-03 02:54:56 +0530

    updated http

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -49,7 +49,7 @@ const CreatePost = () => {
       setLoading(true);
 
       try {
-        const response = await fetch('http://localhost:8000/api/v1/post',
+        const response = await fetch('https://dall-e-ae5t.onrender.com/api/v1/post',
         {
           method: 'POST',
           headers: {
